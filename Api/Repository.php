c086c5d04f6de15b6b19f6779c45c48c228d23ee
Arikaim <sales@intersoft.biz>
2021-01-22 18:32:59 +0200

    =

--- Api/Repository.php
+++ Api/Repository.php
@@ -11,8 +11,6 @@ namespace Arikaim\Core\Api;
 
 use Arikaim\Core\Controllers\ControlPanelApiController;
 use Arikaim\Core\Packages\PackageManager;
-use Arikaim\Core\Db\Model;
-use Arikaim\Core\System\Composer;
 use Arikaim\Core\App\ArikaimStore;
 
 /**
@@ -30,7 +28,6 @@ class Repository extends ControlPanelApiController
         $this->loadMessages('system:admin.messages');
     }
 
-    
     /**
      * Dowload and install repository from repository
      *
@@ -45,6 +42,7 @@ class Repository extends ControlPanelApiController
             $type = $data->get('type',null);
             $package = $data->get('package',null);
             $reposioryName = $data->get('repository',null);
+            $reposioryType = $data->get('repository_type',PackageManager::GITHUB_REPOSITORY);
 
             $packageManager = $this->get('packages')->create($type);
             if (\is_object($packageManager) == false) {
@@ -56,7 +54,8 @@ class Repository extends ControlPanelApiController
 
             $repository = $packageManager->getRepository($package,$accessKey);
             if (empty($repository) == true) {
-                $repository = $packageManager->createRepository($reposioryName,$accessKey);
+                $repoUrl = PackageManager::createRepositoryUrl($reposioryName,$reposioryType);
+                $repository = $packageManager->createRepository($repoUrl,$accessKey);
             }
             if (\is_object($repository) == false) {
                 $this->error('Not valid package name or repository.');
@@ -69,92 +68,12 @@ class Repository extends ControlPanelApiController
             }
 
             $result = $repository->install();
+            
             $this->setResponse($result,function() use($package,$type) {            
                 $this
                     ->message($type . '.download')
                     ->field('type',$type)   
                     ->field('name',$package);                  
-            },'errors.' . $type . '.download');
-        });
-        $data->validate();       
-    }
-
-    /**
-     * Dowload and install package from repository
-     *
-     * @param \Psr\Http\Message\ServerRequestInterface $request
-     * @param \Psr\Http\Message\ResponseInterface $response
-     * @param Validator $data
-     * @return Psr\Http\Message\ResponseInterface
-    */
-    public function repositoryInstallController($request, $response, $data)
-    { 
-        $this->onDataValid(function($data) {  
-            $this->get('cache')->clear();
-            $type = $data->get('type',null);
-            $package = $data->get('package',null);
-            $reposioryType = $data->get('repository_type',null);
-
-            $packageManager = $this->get('packages')->create($type);
-            $repositoryUrl = PackageManager::createRepositoryUrl($package,$reposioryType);
-            $accessKey = null;
-            if ($reposioryType == 'arikaim') {
-                $store = new ArikaimStore();
-                $accessKey = $store->getPackageKey($package);
-            }
-            $repository = $packageManager->createRepository($repositoryUrl,$accessKey);
-
-            $this->get('cache')->clear();
-
-            $result = (\is_object($repository) == true) ? $repository->install() : false;
-
-            $this->setResponse($result,function() use($package,$type) {            
-                $this
-                    ->message($type . '.download')
-                    ->field('type',$type)   
-                    ->field('name',$package);                  
-            },'errors.' . $type . '.download');
-
-        });
-        $data->validate();       
-    }
-
-    /**
-     * Dowload and update package from repository
-     *
-     * @param \Psr\Http\Message\ServerRequestInterface $request
-     * @param \Psr\Http\Message\ResponseInterface $response
-     * @param Validator $data
-     * @return Psr\Http\Message\ResponseInterface
-     */
-    public function repositoryUpdateController($request, $response, $data)
-    {
-        $this->onDataValid(function($data) {  
-            $this->get('cache')->clear();
-
-            $type = $data->get('type',null);
-            $name = $data->get('package',null);
-            $packageManager = $this->get('packages')->create($type);
-
-            if ($type != PackageManager::LIBRARY_PACKAGE) {
-                // create package backup
-                $packageManager->createBackup($name);
-            }
-        
-            $repository = $packageManager->getRepository($name);
-            $result = (\is_object($repository) == true) ? $repository->install() : false;
-            
-            $package = $packageManager->createPackage($name);
-            $version = (\is_object($package) == true) ? $package->getVersion() : null;
-
-            $this->get('cache')->clear();
-            
-            $this->setResponse($result,function() use($name,$type,$version) {            
-                $this
-                    ->message($type . '.download')
-                    ->field('type',$type) 
-                    ->field('type',$version)   
-                    ->field('name',$name);                  
             },'errors.' . $type . '.download');
         });
         $data->validate();       
